795adec7f68855006a588af459161265fe204a04
Maaz Hagalwadi <114414940+Maaz-Hagalwadi@users.noreply.github.com>
2024-06-08 11:42:30 +0530

    Add files via upload

---
+++ Postgres/routes/login.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { Register, Login } = require('../db/dbConection'); // Adjust the path if necessary
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt');
+
+const loginRoute = express.Router();
+
+loginRoute.get("/", (req, res) => {
+  res.send("login page");
+});
+
+loginRoute.post("/", async (req, res) => {
+  const data = req.body;
+  try {
+    const checkUser = await Register.findOne({
+      where: { email: data.email },
+      attributes: ['userName', 'password', 'email']
+    });
+
+    if (checkUser) {
+      bcrypt.compare(data.password, checkUser.password, async (err, result) => {
+        if (!result) {
+          res.status(401).send("Invalid Data");
+        } else {
+          const newUser = await Login.create({
+            ...data,
+            loginId: uuidv4(),
+          });
+
+          if (newUser) {
+            res.send("User logged in successfully");
+          }
+        }
+      });
+    } else {
+      res.status(403).send('Not yet registered');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(401).send("Error while user login");
+  }
+});
+
+module.exports = loginRoute;
